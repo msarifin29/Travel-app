fdc500f258b799ee7cf4c5fbdba2dd944b298756
MS Arifin <ms.arifin29@gmail.com>
2022-11-25 20:05:18 +0800

    init

---
+++ lib/src/const/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/animation.dart';
+
+const Color blueColor = Color(0xff0D6EFD);
+const Color whiteColor = Color(0xffffffff);
+const Color blackBlureColor = Color(0xff2E323E);
+const Color orangeColor = Color(0xffFF7029);
+const Color lightBlackColor = Color(0xff1B1E28);
+const Color greyColor = Color(0xff7D848D);
